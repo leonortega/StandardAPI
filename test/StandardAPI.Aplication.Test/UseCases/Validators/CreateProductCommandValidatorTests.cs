f6432cf6768f00224dec9bd2d7875f9641015422
Leon Ortega <MarceloLeon.Ortega@endava.com>
2025-01-27 17:06:59 -0300

    Add CreateProductCommand validator

---
+++ test/StandardAPI.Aplication.Test/UseCases/Validators/CreateProductCommandValidatorTests.cs
@@ -0,0 +1,57 @@
+﻿using FluentValidation.TestHelper;
+using StandardAPI.Application.DTOs;
+using StandardAPI.Application.UseCases.Validators;
+using Xunit;
+
+namespace StandardAPI.Application.Tests.UseCases.Validators
+{
+    public class CreateProductCommandValidatorTests
+    {
+        private readonly CreateProductCommandValidator _validator;
+
+        public CreateProductCommandValidatorTests()
+        {
+            _validator = new CreateProductCommandValidator();
+        }
+
+        [Fact]
+        public void ShouldHaveErrorWhenNameIsEmpty()
+        {
+            var model = new CreateProductCommandDto { Name = string.Empty, Price = 10 };
+            var result = _validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(x => x.Name).WithErrorMessage("Product name is required.");
+        }
+
+        [Fact]
+        public void ShouldNotHaveErrorWhenNameIsNotEmpty()
+        {
+            var model = new CreateProductCommandDto { Name = "Valid Name", Price = 10 };
+            var result = _validator.TestValidate(model);
+            result.ShouldNotHaveValidationErrorFor(x => x.Name);
+        }
+
+        [Fact]
+        public void ShouldHaveErrorWhenPriceIsZero()
+        {
+            var model = new CreateProductCommandDto { Name = "Valid Name", Price = 0 };
+            var result = _validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(x => x.Price).WithErrorMessage("Price must be greater than zero.");
+        }
+
+        [Fact]
+        public void ShouldHaveErrorWhenPriceIsNegative()
+        {
+            var model = new CreateProductCommandDto { Name = "Valid Name", Price = -1 };
+            var result = _validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(x => x.Price).WithErrorMessage("Price must be greater than zero.");
+        }
+
+        [Fact]
+        public void ShouldNotHaveErrorWhenPriceIsGreaterThanZero()
+        {
+            var model = new CreateProductCommandDto { Name = "Valid Name", Price = 10 };
+            var result = _validator.TestValidate(model);
+            result.ShouldNotHaveValidationErrorFor(x => x.Price);
+        }
+    }
+}
